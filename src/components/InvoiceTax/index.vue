ccedc1e0b5dd5c9bb8d3feb9e3c4c1874bd8718f
Luiz Kim <luiz.kim@controleonline.com>
2024-07-25 18:12:45 -0300

    Update

---
+++ src/components/InvoiceTax/index.vue
@@ -0,0 +1,46 @@
+<template>
+
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  data() {
+    return {
+      items   : [],
+      saving  : false,
+      loading : false,
+    };
+  },
+
+  created() {
+    this.onRequest();
+  },
+
+  computed: {
+    ...mapGetters({
+      myCompany: 'people/currentCompany',
+    }),
+
+    user() {
+      return this.$store.getters['auth/user'];
+    },
+  },
+
+  watch: {
+    myCompany(company) {
+      this.onRequest();
+    },
+  },
+
+  methods: {
+    onRequest() {
+      let params = {};
+      let items  = [];
+
+      this.items = items;
+    },
+  },
+};
+</script>
